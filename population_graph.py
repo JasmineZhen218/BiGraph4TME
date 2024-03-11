255d446d60e3643888807120f684a27ed4a30c11
JasmineZhen218 <zwang218@jhu.edu>
2024-03-11 13:32:24 -0400

    Estimate the subgroup belongings of new patients using knn rule

--- population_graph.py
+++ population_graph.py
@@ -3,8 +3,9 @@ import networkx as nx
 
 
 class Population_Graph:
-    def __init__(self, k=20, resolution=1.0, size_smallest_cluster=10, seed=1):
-        self.k = k # number of nearest neighbors in the population graph
+    def __init__(self, k_clustering=20, k_estimate = 3, resolution=1.0, size_smallest_cluster=10, seed=1):
+        self.k_clustering = k_clustering # decides the coarseness of patient clustering
+        self.k_estimate = k_estimate # number of nearest neighbors for estimating the community
         self.resolution = resolution # resolution parameter for community detection
         self.seed = seed # random seed
         self.size_smallest_cluster = size_smallest_cluster # the size of the smallest cluster
@@ -32,7 +33,7 @@ class Population_Graph:
             idx = np.argsort(Similarity_matrix_[i])[
                 ::-1
             ]  # sort the similarity in descending order
-            Similarity_matrix_[i, idx[self.k :]] = (
+            Similarity_matrix_[i, idx[self.k_clustering :]] = (
                 0  # remove edges that are not in the k nearest neighbors
             )
             adj_1 = np.maximum(
@@ -75,6 +76,45 @@ class Population_Graph:
             Patient_subgroups[i] = [G_population.nodes[n]["patientID"] for n in c] # fill in the patient subgroups
         return Patient_subgroups
     
-    def estimate_community(self,  Similarity_matrix, Patient_subgroups_reference):
+    def estimate_community(self,  Patient_ids_train, Patient_ids_hat, Patient_subgroups_train, Similarity_matrix):
+        """
+        Estimate the patient subgroups for the new patients.
+        Parameters
+        ----------
+        Patient_ids_train : list of str
+            The patient ids in the training set.
+        Patient_ids_hat : list of str
+            The patient ids in the test set.
+        Patient_subgroups_train : dict
+            The patient subgroups in the training set. The key is the subgroup id, and the value is a list of patient ids.
+        Similarity_matrix : numpy array (n_patients_train, n_patients_hat)
+            The similarity matrix between the training set and the test set.
+        Returns
+        -------
+        Patient_subgroups_hat : dict
+            The estimated patient subgroups. The key is the subgroup id, and the value is a list of patient ids.
+    
+        """
+        Labels_train = np.zeros(Similarity_matrix.shape[0], dtype=int)
+        Labels_hat = np.zeros(Similarity_matrix.shape[1], dtype=int)
+        for group_id in Patient_subgroups_train:
+            for patient_id in Patient_subgroups_train[group_id]:
+                Labels_train[Patient_ids_train.index(patient_id)] = group_id
+        for new_patient in range(Similarity_matrix.shape[1]):
+            idx = np.argsort(Similarity_matrix[:, new_patient])[
+                ::-1
+            ]
+            knn_idx = idx[:self.k_estimate]
+            knn_labels = Labels_train[knn_idx]
+            knn_similarities = Similarity_matrix[knn_idx, new_patient]
+            unique, counts = np.unique(knn_labels, return_counts=True)
+            similarities_within_knn = np.zeros(len(unique))
+            for j in range(len(unique)):
+                similarities_within_knn[j] = np.sum(knn_similarities[knn_labels == unique[j]])
+            Labels_hat[new_patient] = unique[np.argmax(similarities_within_knn)]
+        Patient_subgroups_hat = {i: [] for i in range(len(Patient_subgroups_train))}
+        for i in range(len(Labels_hat)):
+            Patient_subgroups_hat[int(Labels_hat[i])].append(Patient_ids_hat[i])
+        return Patient_subgroups_hat
         
 
